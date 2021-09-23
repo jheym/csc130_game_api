d3b42ebac9d9effdc3d9751ad8c9a5bf5913fe43
jheym <jheym@graybox.mshome.net>
2021-09-23 02:40:06 -0700

    fixed sprite movement and changed comments

--- CSC_130_Final/src/Main/Main.java
+++ CSC_130_Final/src/Main/Main.java
@@ -1,5 +1,5 @@
 // Author: 				Justin Heyman
-// Date: 				2021-09-22
+// Date: 				
 // Assignment: 			
 // Note to Professor: 	
 
@@ -18,10 +18,10 @@ public class Main{
 	// Fields (Static) below...
 	public static Color c = new Color(255, 255, 51);
 	public static boolean isImageDrawn = false;
-	public static stopWatchX timer = new stopWatchX(15); // 15 ms should look like 60 fps
+	public static stopWatchX timer = new stopWatchX(15); // modify timer here -- 15 ms looks like 66.7 fps -- when does gameloop bottleneck?
 	public static Queue<Vector2D> vecs1 = new LinkedList<>();
 	public static Queue<Vector2D> vecs2 = new LinkedList<>();
-	public static Vector2D currentVec = new Vector2D(-128, 320);
+	public static Vector2D currentVec = new Vector2D(-128, 296); // -128, 296 is just off the screen and in the middle ((720/2) - (128/2) = 296)
 	// End Static fields...
 	
 	public static void main(String[] args) {
@@ -30,13 +30,11 @@ public class Main{
 	}
 	
 	/* This is your access to things BEFORE the game loop starts */
-	public static void start(){
-		// TODO: Code your starting conditions here...NOT DRAW CALLS HERE! (no addSprite or drawString)
-		
+	public static void start() {
 		// initial vec1 queue population
-		while (currentVec.getX() < 1280){
-			vecs1.add(new Vector2D(currentVec.getX(),currentVec.getY())); // Adds a new <Vector2D> type object to the queue
-			currentVec.adjustX(5); // moves the new vector object 5 pixels to the right each iteration
+		while (currentVec.getX() <= 1280){ // populates the queue with vector objects that go up to 1280 px
+			vecs1.add(new Vector2D(currentVec.getX(),currentVec.getY())); // Adds a new <Vector2D> type object to the queue with the current coords
+			currentVec.adjustX(4); // will increase coords of next object by 4 px
 		}		
 	}
 	
@@ -49,7 +47,7 @@ public class Main{
 			vecs1 = vecs2;
 		}
 		
-		if(timer.isTimeUp()){ // this is a time-based loop that updates the sprite position every 15 milliseconds (can modify the timer in static fields)
+		if(timer.isTimeUp()){ // this is a time-based condition that updates the sprite position every 15 milliseconds (can modify the timer in static fields)
 			vecs1.remove(); 
 			vecs2.add(currentVec); 
 			currentVec = vecs1.peek();
